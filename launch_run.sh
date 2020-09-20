6fa2e34ac8136799248ade64fb322136a77c2a8d
ixobert <axelchristianguei@gmail.com>
2020-09-20 15:09:32 -0400

    First run works

--- launch_run.sh
+++ launch_run.sh
@@ -3,7 +3,7 @@ COMMIT_MESSAGE=${@}
 echo Current branch: $CURRENT_BRANCH
 echo Run message: $COMMIT_MESSAGE
 git commit -am "$COMMIT_MESSAGE"
-# git checkout runs
-# git merge $CURRENT_BRANCH
-# git push
-# git checkout $CURRENT_BRANCH
+git checkout runs
+git merge $CURRENT_BRANCH
+git push
+git checkout $CURRENT_BRANCH
