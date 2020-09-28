3c55790afd4ef81246d3e63cdd738225a7dda77a
ixobert <axelchristianguei@gmail.com>
2020-09-28 00:15:53 -0400

    debug

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd src/
-/home/future/anaconda3/envs/scologan/bin/python src/train_vqvae.py +nb_epochs=10 +gpus=[0]
+/home/future/anaconda3/envs/scologan/bin/python src/train_vqvae.py nb_epochs=10 +gpus=[0]
 
 cd ..
