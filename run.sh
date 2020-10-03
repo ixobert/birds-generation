f13a51237348855bdca90529aa863b7958585c2b
ixobert <axelchristianguei@gmail.com>
2020-10-02 23:55:17 -0400

    Init Training: fix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 set -e 0
 echo $(pwd)
-# /home/future/anaconda3/envs/scologan/bin/python ./src/train_vqvae.py +gpus=[0] debug=false dataset="taylor-fix" run_name="Init Train" tags=["vq-vae2", "taylor_fix"] &\
-# sleep 3 &\
+/home/future/anaconda3/envs/scologan/bin/python ./src/train_vqvae.py +gpus=[0] debug=false dataset="taylor-fix" run_name="Init Train" tags=["vq-vae2", "taylor_fix"] &\
+sleep 3 &\
 /home/future/anaconda3/envs/scologan/bin/python ./src/train_vqvae.py +gpus=[1] debug=false dataset="udem-birds" run_name="Init Train" tags=["vq-vae2", "full_6_udem"]
