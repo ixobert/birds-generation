96db24703f63f4759b2a105a289c8340b3224030
ixobert <axelchristianguei@gmail.com>
2020-10-02 22:58:08 -0400

    Init Train

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 set -e 0
 echo $(pwd)
-/home/future/anaconda3/envs/scologan/bin/python ./src/train_vqvae.py nb_epochs=10 +gpus=[0] debug=false tags=['debug']
+/home/future/anaconda3/envs/scologan/bin/python ./src/train_vqvae.py +gpus=[0] debug=false tags=['debug']
