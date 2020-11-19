e08eef85d15126408050087dfd0ccdbe28a369d0
ixobert <axelchristianguei@gmail.com>
2020-11-18 21:12:11 -0500

    Debug: Extract latents

--- src/dataloaders/audiodataset.py
+++ src/dataloaders/audiodataset.py
@@ -153,7 +153,7 @@ class AudioDataset():
 
     def __getitem__(self, idx):
         file_path, audio = self.data[idx]
-        logging.info(file_path)
+        logging.info(f"**{file_path}**")
         if len(audio) >= self.window_length:
             audio = audio[0:self.window_length]
         else:
