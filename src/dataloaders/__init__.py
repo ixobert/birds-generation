8a6a8fbe1ae8a62e4e80bdfd94714d100c4c7be9
ixobert <axelchristianguei@gmail.com>
2021-04-16 01:05:09 -0400

    Train-Classifier:NIPS4BPlus- ImageBase vs noAug

--- src/dataloaders/__init__.py
+++ src/dataloaders/__init__.py
@@ -1,4 +1,5 @@
 import os
+from albumentations.augmentations.transforms import RandomScale
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 import torch
@@ -75,7 +76,10 @@ class SpectrogramsDataModule(pl.LightningDataModule):
         transforms = self.config.get('transforms', None)
         if transforms == "image_base":
             transforms = A.Compose([
-                    A.RandomBrightnessContrast(p=0.2),
+                    A.VerticalFlip(),
+                    A.HorizontalFlip(),
+                    A.RandomScale(),
+                    A.Rotate(limit=10),
                 ])
 
         self.dataset      = AudioDataset(data_path=self.config['train_path'], root_dir=self.config['root_dir'], classes_name=self.config['classes_name'], sr=self.config['sr'], window_length=self.config['sr']*4, spec=self.config['use_mel'], resize=self.config['resize'], return_tuple=self.config['return_tuple'], return_tuple_of3=self.config.get('return_tuple_of3', True), use_spectrogram=self.config.get('use_mel', False), use_cache=self.config.get('use_cache', True), use_rgb=self.config.get('use_rgb', False), transforms=transforms)
