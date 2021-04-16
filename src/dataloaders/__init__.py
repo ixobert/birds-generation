77d4a65da3e606d14fa6e95307988b1820803124
ixobert <axelchristianguei@gmail.com>
2021-04-16 01:13:47 -0400

    Train-Classifier:NIPS4BPlus- ImageBase vs noAug

--- src/dataloaders/__init__.py
+++ src/dataloaders/__init__.py
@@ -79,7 +79,6 @@ class SpectrogramsDataModule(pl.LightningDataModule):
                     A.VerticalFlip(),
                     A.HorizontalFlip(),
                     A.RandomScale(),
-                    A.Rotate(limit=10),
                 ])
 
         self.dataset      = AudioDataset(data_path=self.config['train_path'], root_dir=self.config['root_dir'], classes_name=self.config['classes_name'], sr=self.config['sr'], window_length=self.config['sr']*4, spec=self.config['use_mel'], resize=self.config['resize'], return_tuple=self.config['return_tuple'], return_tuple_of3=self.config.get('return_tuple_of3', True), use_spectrogram=self.config.get('use_mel', False), use_cache=self.config.get('use_cache', True), use_rgb=self.config.get('use_rgb', False), transforms=transforms)
